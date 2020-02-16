5dc023931da71fe770f794a1bdc00d614603f7ca
Sergey Pomortsev <zaooooza@gmail.com>
2020-02-16 21:14:47 +0300

    add project

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -4,7 +4,7 @@ import classes from "./Friends.module.css";
 
 const Friends = (props) => {
   let friendElement = props.state
-  .map(f => <div> ID : {f.id} ,Имя: {f.name} ,Возраст {f.age} </div> );
+  .map(f => <div> Имя: {f.name} ,Возраст {f.age} </div> );
   return (
     <div>
       {friendElement}
